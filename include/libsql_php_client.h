e0ec5163421c0eaa613f2e7d9807694f7452dd5b
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-04-21 19:26:58 +0700

    refactor: Update libsql_php_client.h and libsql_php.def for consistency in function naming

--- include/libsql_php_client.h
+++ include/libsql_php_client.h
@@ -2,10 +2,6 @@
 
 void libsql_php_close(void *client_ptr);
 
-LocalClient *libsql_php_open_file(const char *path);
+Connection *libsql_php_connect_local(const char *path);
 
 const char *libsql_php_query(void *client_ptr, const char *query);
-
-const int *libsql_php_exec(void *client_ptr, const char *query);
-
-int libsql_php_sync(void *client_ptr);
