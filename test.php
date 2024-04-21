757b43786241fc17ae1c8fb7c29d8933b81e6df0
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-04-21 19:25:30 +0700

    refactor: Update test.php with additional database operations and result outputs

--- test.php
+++ test.php
@@ -6,17 +6,26 @@ require_once 'vendor/autoload.php';
 
 $db = new LibsqlPHP("database.db");
 if ($db->is_connected()) {
+
+    $db->exec("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)");
+    $db->exec("INSERT INTO users (name) VALUES ('Tokio')");
+    $db->exec("INSERT INTO users (name) VALUES ('Serde')");
+    $db->exec("INSERT INTO users (name) VALUES ('Elpha')");
+
     $result = $db->query("SELECT * FROM users");
     
-    // echo "Return as raw:" . PHP_EOL;
-    // var_dump($result->fetchRaw());
+    echo "Return as raw:" . PHP_EOL;
+    var_dump($result->fetchRaw());
     
     echo "Return as associative array:" . PHP_EOL;
-    var_dump($result->fetchArray(SQLITE3_ASSOC));
+    var_dump($result->fetchArray(SQLITE3_NUM));
+
+    echo "Return the column count:" . PHP_EOL;
+    var_dump($result->numColumns());
     
-    // echo "Return the column names:" . PHP_EOL;
-    // var_dump($result->columName());
+    echo "Return the column names:" . PHP_EOL;
+    var_dump($result->columName());
     
-    // echo "Return the column types:" . PHP_EOL;
-    // var_dump($result->columnType());
+    echo "Return the column types:" . PHP_EOL;
+    var_dump($result->columnType());
 }
