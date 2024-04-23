3fe2b54bbcf01c65e16ec8c7bb8d7267aebe9d0c
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-04-23 15:16:39 +0700

    feat: Add additional parameters to libsql_php_connect_local function

--- include/libsql_php_client.h
+++ include/libsql_php_client.h
@@ -2,6 +2,16 @@
 
 void libsql_php_close(void *client_ptr);
 
-Connection *libsql_php_connect_local(const char *path);
+Connection *libsql_php_connect_local(const char *path,
+                                     const char *flags,
+                                     const char *encryption_key);
 
 const char *libsql_php_query(void *client_ptr, const char *query);
+
+const int64_t *libsql_php_exec(void *client_ptr, const char *query);
+
+const uint64_t *libsql_php_affected_rows(void *client_ptr);
+
+const int *libsql_php_reset(void *client_ptr);
+
+const char *libsql_version(void);
