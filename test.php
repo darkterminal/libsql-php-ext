dc908cdd26568e15194842597fa6bf0a0fd49636
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-04-24 23:32:03 +0700

    feat: Add transaction and execute_batch support to test.php

--- test.php
+++ test.php
@@ -1,46 +1,52 @@
 <?php
 
 use Darkterminal\LibSQLPHPExtension\LibSQLPHP;
+use Darkterminal\LibSQLPHPExtension\Utils\TransactionBehavior;
 
 require_once 'vendor/autoload.php';
 
 $db = new LibSQLPHP("file:database.db");
 if ($db->is_connected()) {
+
     echo $db->version() . PHP_EOL;
 
     // $db->exec("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)");
     // $db->exec("INSERT INTO users (name) VALUES ('Handoko')");
     // $db->exec("INSERT INTO users (name) VALUES ('Karlina')");
-    // $db->exec("INSERT INTO users (name) VALUES ('Carla')");
 
-    // $result = $db->query("SELECT * FROM users");
-    
+    // $db->exec("INSERT INTO users (name) VALUES (?)", ["Marlo"]);
+    // var_dump($db->last_insert_rowid());
+
+    // $result = $db->query("SELECT * FROM users LIMIT 5");
+
     // echo "Return as raw:" . PHP_EOL;
     // var_dump($result->fetchRaw());
-    
-    // echo "Return as associative array:" . PHP_EOL;
-    // $users = $result->fetchArray(LIBSQLPHP_ASSOC);
+
+    // echo "Return as default (LIBSQLPHP_NUM):" . PHP_EOL;
+    // $users = $result->fetchArray(LIBSQLPHP_NUM);
+
+    // var_dump($users);
     // foreach ($users as $user) {
     //     echo $user['name'] . PHP_EOL;
     // }
 
     // echo "Return the column count:" . PHP_EOL;
     // var_dump($result->numColumns());
-    
+
     // echo "Return the column names:" . PHP_EOL;
     // var_dump($result->columName());
-    
+
     // echo "Return the column types:" . PHP_EOL;
     // var_dump($result->columnType());
 
-    // $stmt = $db->prepare("INSERT INTO foo VALUES (:baz, @foo)");
+    // $stmt = $db->prepare("INSERT INTO persons (name, age) VALUES (:name, @age)");
 
     // // Bind parameters
-    // $stmt->bindParam(':baz', $baz, LIBSQLPHP_TEXT);
-    // $stmt->bindParam('@foo', $foo, LIBSQLPHP_INTEGER);
-    // $baz = "baz";
-    // $foo = 2;
-    // echo $stmt->execute() . PHP_EOL;
+    // $stmt->bindParam(':name', $baz, LIBSQLPHP_TEXT);
+    // $stmt->bindParam('@age', $foo, LIBSQLPHP_INTEGER);
+    // $baz = "Sarah";
+    // $foo = 22;
+    // $stmt->execute();
 
     // $stmt = $db->prepare("INSERT INTO foo VALUES (?, ?)");
 
@@ -70,5 +76,25 @@ if ($db->is_connected()) {
     // $result2 = $db->querySingle("SELECT name FROM users WHERE id = 2", true);
     // var_dump($result);
     // var_dump($result2);
+
+    // $operations_successful = false;
+    // $tx = $db->transaction(TransactionBehavior::Deferred);
+    // $tx->exec("INSERT INTO users (name) VALUES (?)", ["Emanuel"]);
+    // $tx->exec("INSERT INTO users (name) VALUES (?)", ["Darren"]);
+
+    // if ($operations_successful) {
+    //     $tx->commit();
+    //     echo "Commit the changes" . PHP_EOL;
+    // } else {
+    //     $tx->rollback();
+    //     echo "Rollback the changes" . PHP_EOL;
+    // }
+
+    // $db->execute_batch("
+    //     BEGIN;
+    //     CREATE TABLE foo(x INTEGER);
+    //     CREATE TABLE bar(y TEXT);
+    //     COMMIT;
+    // ");
 }
 $db->close();
