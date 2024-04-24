fbe9c21dcbcaf694c5ada5223c897d3cafb40cf9
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-04-24 23:33:17 +0700

    feat: Add support for batch execution and transaction management

--- include/libsql_php_client.h
+++ include/libsql_php_client.h
@@ -8,10 +8,30 @@ Connection *libsql_php_connect_local(const char *path,
 
 const char *libsql_php_query(void *client_ptr, const char *query);
 
-const int64_t *libsql_php_exec(void *client_ptr, const char *query);
+const int64_t *libsql_php_exec(void *client_ptr,
+                               const char *query,
+                               const char *const *query_params,
+                               uintptr_t query_params_len);
+
+const int *libsql_php_execute_batch(void *client_ptr, const char *query);
 
 const uint64_t *libsql_php_affected_rows(void *client_ptr);
 
 const int *libsql_php_reset(void *client_ptr);
 
 const char *libsql_version(void);
+
+int64_t libsql_php_is_autocommit(void *client_ptr);
+
+int64_t libsql_php_last_insert_rowid(void *client_ptr);
+
+Transaction *libsql_php_transaction(void *client_ptr, const char *behavior);
+
+Transaction *libsql_php_transaction_exec(void *trx_ptr,
+                                         const char *query,
+                                         const char *const *query_params,
+                                         uintptr_t query_params_len);
+
+int64_t libsql_php_transaction_commit(void *trx_ptr);
+
+int64_t libsql_php_transaction_rollback(void *trx_ptr);
