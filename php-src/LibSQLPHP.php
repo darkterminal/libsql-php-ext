4674f04adc41a31202cdcdd7c82a18282b07005a
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-04-27 16:07:12 +0700

    feat: Enhancements and fixes in LibSQLPHP

--- php-src/LibSQLPHP.php
+++ php-src/LibSQLPHP.php
@@ -10,6 +10,7 @@
 
 namespace Darkterminal\LibSQLPHPExtension;
 
+use Darkterminal\LibSQLPHPExtension\Responses\LibSQLPHPClientResult;
 use Darkterminal\LibSQLPHPExtension\Responses\LibSQLPHPResult;
 use Darkterminal\LibSQLPHPExtension\Responses\LibSQLPHPStmt;
 use Darkterminal\LibSQLPHPExtension\Responses\Transaction;
@@ -51,15 +52,28 @@ class LibSQLPHP
 
     /**
      * Constructor.
+     * 
+     * The constructor of the LibSQLPHP class ensures that the provided
+     * parameters are valid, checks for the existence of necessary files, initializes
+     * the FFI interface for interacting with the LibSQL library, and opens a connection to the database. 
+     * It encapsulates the initialization logic required for setting up a new instance of the LibSQLPHP class.
+     * 
+     * **Example**
+     * 
+     * ```
+     * $db = new LibSQLPHP("file:database.db");
+     * $db = new LibSQLPHP(":memory:");
+     * $db = new LibSQLPHP();
+     * ```
      *
      * @param string $path Path to the database file.
-     * @param int $flags Flags to control database opening mode.
+     * @param int $flags Flags to control database opening mode. Default: LIBSQLPHP_OPEN_READWRITE | LIBSQLPHP_OPEN_CREATE
      * @param string $encryptionKey Encryption key for database (if applicable).
      *
      * @throws \Exception If invalid flags are provided.
      */
     public function __construct(
-        string $path,
+        string $path = "",
         int $flags = LIBSQLPHP_OPEN_READWRITE | LIBSQLPHP_OPEN_CREATE,
         string $encryptionKey = ""
     ) {
@@ -71,7 +85,7 @@ class LibSQLPHP
         if (!file_exists(__DIR__ . '/libsql_php.def') && !file_exists(__DIR__ . '/../libs/libsql_php_client.so')) {
             throw new \Exception("LibSQLPHP definition and extension is not exits!");
         }
-        
+
         // Load the FFI interface
         $this->ffi = \FFI::cdef(
             file_get_contents(__DIR__ . '/libsql_php.def'),
@@ -84,6 +98,13 @@ class LibSQLPHP
 
     /**
      * Open a connection to a local database.
+     * 
+     * **Example**
+     * 
+     * ```
+     * $db = new LibSQLPHP();
+     * $db->open("file:database.db");
+     * ```
      *
      * @param string $path The path to the local database file.
      * @param int $flags Flags to control database opening mode.
@@ -95,16 +116,29 @@ class LibSQLPHP
      */
     public function open(string $path, int $flags, string $encryptionKey = ""): void
     {
-        $conn = $this->checkConnectionMode($path);
-        if ($conn === false || $conn['mode'] !== 'local') {
-            throw new \Exception("Error: Connection failed available mode: Local");
+        // Check if provided flags are allowed
+        if ($this->checkFlags($flags) === false) {
+            throw new \Exception("LibSQLPHP Flags are not allowed to be used");
         }
+
+        $conn = $this->checkConnectionMode($path);
+        if ($conn === false || !in_array($conn['mode'], ['local', 'memory'])) {
+            throw new \Exception("Error: Connection failed available mode: Local or in-momory");
+        }
+
         $this->db = $this->ffi->libsql_php_connect_local($conn['uri'], $this->checkFlags($flags), $encryptionKey);
         $this->is_connected = ($this->db) ? true : false;
     }
 
     /**
      * Checks whether the database connection is in autocommit mode.
+     * 
+     * **Example**
+     * 
+     * ```
+     * $db = new LibSQLPHP("file:database.db");
+     * $db->is_autocommit();
+     * ```
      *
      * @return bool True if the database connection is in autocommit mode, false otherwise.
      */
@@ -115,6 +149,13 @@ class LibSQLPHP
 
     /**
      * Check if a connection to the database is established.
+     * 
+     * **Example**
+     * 
+     * ```
+     * $db = new LibSQLPHP("file:database.db");
+     * $db->is_connected();
+     * ```
      *
      * @return bool True if connected, false otherwise.
      */
@@ -125,6 +166,16 @@ class LibSQLPHP
 
     /**
      * Close the connection to the database.
+     * 
+     * **Example**
+     * 
+     * ```
+     * $db = new LibSQLPHP("file:database.db");
+     * if ($db->is_connected()) {
+     *     echo $db->version() . PHP_EOL;
+     * }
+     * $db->close(); // Always close the database connection
+     * ```
      *
      * @return void
      */
@@ -137,37 +188,66 @@ class LibSQLPHP
 
     /**
      * Execute a query on the database.
+     * 
+     * **Example**
+     * 
+     * ```
+     * $result = $db->query("SELECT * FROM users LIMIT 5");
+     * ```
      *
      * @param string $stmt The SQL statement to execute.
+     * @param array $params The SQL statement to parameters.
      *
      * @return LibSQLPHPResult The result of the query.
      */
-    public function query(string $stmt): LibSQLPHPResult
+    public function query(string $stmt, array $params = []): LibSQLPHPResult
     {
-        $data = $this->ffi->libsql_php_query($this->db, $stmt);
+        if (is_array_assoc($params)) {
+            $queryParams = new QueryParams([]);
+            $intoParams = intoParams($stmt, $params);
+            var_dump($intoParams);
+            $data = $this->ffi->libsql_php_query($this->db, $intoParams, $queryParams->getData(), $queryParams->getLength());
+            $queryParams->freeParams();
+        } else {
+            $queryParams = new QueryParams($params);
+            $data = $this->ffi->libsql_php_query($this->db, $stmt, $queryParams->getData(), $queryParams->getLength());
+            $queryParams->freeParams();
+        }
         $object = json_decode($data, true);
         return new LibSQLPHPResult($this->ffi, $this->db, $object);
     }
 
     /**
      * Execute a query and retrieve a single result from the database.
+     * 
+     * **Example**
+     * 
+     * ```
+     * $result = $db->querySingle("SELECT name FROM users WHERE id = 1");
+     * $result2 = $db->querySingle("SELECT name FROM users WHERE id = 2", true);
+     * ```
      *
      * @param string $stmt The SQL statement to execute.
+     * @param string $params The SQL statement parameters to execute.
      * @param bool $entireRow Whether to return the entire row or a single value.
      *
      * @return array|string The result of the query.
      *
      * @throws \Exception If the query is not designed for single results.
      */
-    public function querySingle(string $stmt, bool $entireRow = false): array|string
+    public function querySingle(string $stmt, array $params = [], bool $entireRow = false): array|string
     {
         $end = substr($stmt, -5);
         if (strpos($stmt, 'WHERE') !== false && $end !== 'WHERE') {
-            $data = $this->ffi->libsql_php_query($this->db, $stmt);
+            $queryParams = new QueryParams($params);
+            $data = $this->ffi->libsql_php_query($this->db, $stmt, $queryParams->getData(), $queryParams->getLength());
+            $queryParams->freeParams();
+
             $object = json_decode($data, true);
             $result = new LibSQLPHPResult($this->ffi, $this->db, $object);
             $handle = current($result->fetchArray(LIBSQLPHP_ASSOC));
             $arr = array_map(fn ($value) => $value, array_values($handle));
+
             if ($entireRow !== true && count($arr) === 1) {
                 $result = $arr[0];
             } else {
@@ -181,6 +261,24 @@ class LibSQLPHP
 
     /**
      * Prepare a SQL statement for execution.
+     * 
+     * **Example**
+     * 
+     * ```
+     * // Bind Param
+     * $stmt = $db->prepare("INSERT INTO persons (name, age) VALUES (:name, @age)");
+     * $stmt->bindParam(':name', $baz, LIBSQLPHP_TEXT);
+     * $stmt->bindParam('@age', $foo, LIBSQLPHP_INTEGER);
+     * $baz = "Sarah";
+     * $foo = 22;
+     * $stmt->execute();
+     * 
+     * // Bind Value
+     * $stmt = $db->prepare("INSERT INTO foo VALUES (?, ?)");
+     * $stmt->bindValue(1, "baz", LIBSQLPHP_TEXT);
+     * $stmt->bindValue(2, 5, LIBSQLPHP_INTEGER);
+     * $stmt->execute();
+     * ```
      *
      * @param string $query The SQL query to prepare.
      *
@@ -193,6 +291,15 @@ class LibSQLPHP
 
     /**
      * Get the number of rows affected by the last operation.
+     * 
+     * **Example**
+     * 
+     * ```
+     * $db->exec("DELETE FROM users WHERE id = 3");
+     * 
+     * $changes = $db->changes();
+     * echo "The DELETE statement removed $changes rows";
+     * ```
      *
      * @return int The number of affected rows.
      */
@@ -214,6 +321,24 @@ class LibSQLPHP
 
     /**
      * Initiates a database transaction with the specified behavior.
+     * 
+     * **Example**
+     * 
+     * ```
+     * $operations_successful = false;
+     * $tx = $db->transaction(TransactionBehavior::Deferred);
+     * $tx->exec("INSERT INTO users (name) VALUES (?)", ["Emanuel"]);
+     * $tx->exec("INSERT INTO users (name) VALUES (?)", ["Darren"]);
+     * 
+     * if ($operations_successful) {
+     *     $tx->commit();
+     *     echo "Commit the changes" . PHP_EOL;
+     * } else {
+     *     $tx->rollback();
+     *     echo "Rollback the changes" . PHP_EOL;
+     * }
+     * ```
+     * NOTE: After `commit` or `rollback` the `$tx` will be free from memory
      *
      * @param string $behavior The behavior of the transaction (optional, defaults to TransactionBehavior::Deferred).
      *
@@ -226,9 +351,17 @@ class LibSQLPHP
 
     /**
      * Execute a SQL statement.
+     * 
+     * **Example**
+     * 
+     * ```
+     * $db->exec("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)");
+     * $db->exec("INSERT INTO users (name) VALUES ('Handoko')");
+     * $db->exec("INSERT INTO users (name) VALUES ('Karlina')");
+     * ```
      *
      * @param string $query The SQL statement to execute.
-     * @param array $params The SQL parameters to execute.
+     * @param array $params The SQL parameters to execute. Optional
      *
      * @return bool True if the execution was successful, false otherwise.
      */
@@ -239,6 +372,29 @@ class LibSQLPHP
         $queryParams->freeParams();
         return $exec[0] > 0;
     }
+
+    /**
+     * Executes a client query and returns the result. - LibSQL Client PHP Usage Only
+     *
+     * This method executes a client query using the provided SQL statement and parameters.
+     * It then creates a new LibSQLPHPClientResult instance based on the query result.
+     *
+     * @param string $query The SQL query statement to execute.
+     * @param array $params The parameters to bind to the SQL query.
+     * @return LibSQLPHPClientResult The result of the client query.
+     */
+    public function client_exec(string $query, array $params): LibSQLPHPClientResult
+    {
+        $data = $this->query($query, $params);
+        return new LibSQLPHPClientResult(
+            $data->columName(),
+            $data->columnType(),
+            $data->fetchArray(LIBSQLPHP_ASSOC),
+            $this->changes(),
+            $this->last_insert_rowid()
+        );
+    }
+
 
     /**
      * Executes a batch of SQL queries.
@@ -270,7 +426,7 @@ class LibSQLPHP
     }
 
     /**
-     * Get the version of the LibSQL library.
+     * Get the version of the LibSQL Binary.
      *
      * @return string The version string.
      */
@@ -345,6 +501,12 @@ class LibSQLPHP
             $path = [
                 'mode' => $this->connection_mode,
                 'uri' => str_replace("file:", "", $path)
+            ];
+        } else if ($path === ":memory:") {
+            $this->connection_mode = 'memory';
+            $path = [
+                'mode' => $this->connection_mode,
+                'uri' => $path
             ];
         } else {
             $path = false;
